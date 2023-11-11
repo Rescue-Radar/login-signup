d91f0febd8a29a13a9775acd0d80daa2d86b2d0e
Ankit kumar <ankit.kumar.info22@gmail.com>
2023-11-11 20:45:30 +0530

    readme added

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost', // Your PostgreSQL host
-//     database: 'rescueradar',
-//     password: 'root',
-//     port: 5432, // Your PostgreSQL port
-//   //   ssl: {
-// 	// 	rejectUnauthorized: true,
-// 	// },
-//   });
 const pool = new pg_1.Pool({
-    connectionString: "postgres://default:wghjQmzZ93tU@ep-super-union-40832357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",
+    user: 'postgres',
+    host: 'localhost',
+    database: 'rescueradar',
+    password: 'root',
+    port: 5432, // Your PostgreSQL port
+    //   ssl: {
+    // 	rejectUnauthorized: true,
+    // },
 });
+// const pool = new Pool({
+//   connectionString: "postgres://default:wghjQmzZ93tU@ep-super-union-40832357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",
+// })
 pool
     .connect()
     .then(() => {
--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -2,21 +2,21 @@
 import { Pool } from 'pg';
  
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost', // Your PostgreSQL host
-//     database: 'rescueradar',
-//     password: 'root',
-//     port: 5432, // Your PostgreSQL port
-//   //   ssl: {
-// 	// 	rejectUnauthorized: true,
-// 	// },
-//   });
-
-
 const pool = new Pool({
-  connectionString: "postgres://default:wghjQmzZ93tU@ep-super-union-40832357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",
-})
+    user: 'postgres',
+    host: 'localhost', // Your PostgreSQL host
+    database: 'rescueradar',
+    password: 'root',
+    port: 5432, // Your PostgreSQL port
+  //   ssl: {
+	// 	rejectUnauthorized: true,
+	// },
+  });
+
+
+// const pool = new Pool({
+//   connectionString: "postgres://default:wghjQmzZ93tU@ep-super-union-40832357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",
+// })
 
 
   pool
